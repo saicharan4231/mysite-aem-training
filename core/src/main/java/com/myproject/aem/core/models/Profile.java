e54767c121a35546fa19e0df205d24476b8ed67c
Sai Charan <saicharankoppalli@gmail.com>
2024-03-06 13:15:54 +0530

    initial commit

---
+++ core/src/main/java/com/myproject/aem/core/models/Profile.java
@@ -0,0 +1,11 @@
+package com.myproject.aem.core.models;
+
+public interface Profile {
+
+    String getFirstName();
+    String getLastName();
+    String getProfileDesc();
+    Long getAge();
+    String getDepartment();
+
+}
