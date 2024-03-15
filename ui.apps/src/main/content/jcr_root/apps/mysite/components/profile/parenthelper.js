dceb67492e1817322a4aef04552e95ceacb27ae6
Sai Charan <saicharankoppalli@gmail.com>
2024-03-15 19:50:11 +0530

    HTL Practice

---
+++ ui.apps/src/main/content/jcr_root/apps/mysite/components/profile/parenthelper.js
@@ -0,0 +1,7 @@
+
+use(function(){
+	var info = {};
+	info.title = currentPage.parent.title;
+	info.name = currentPage.parent.name;
+	return info;
+});
---
+++ ui.apps/src/main/content/jcr_root/apps/mysite/components/profile/stringhelper.js
@@ -0,0 +1,7 @@
+
+use(function(){
+	var info = {};
+	info.length = this.str.length;
+    return info;
+});
+
